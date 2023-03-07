06aeeb095dd68c4c37fabe2967624abbfa1e96d2
Hoang Hy <hoanghy01122003@gmail.com>
2023-03-07 23:12:03 +0700

    edit connection string configuration

--- src/config.ts
+++ src/config.ts
@@ -4,16 +4,12 @@ export const port = process.env.PORT;
 export const timezone = process.env.TZ;
 
 export const db = {
-  name: process.env.DB_NAME || '',
-  host: process.env.DB_HOST || '',
-  port: process.env.DB_PORT || '',
-  user: process.env.DB_USER || '',
-  password: process.env.DB_USER_PWD || '',
+  connectionString: process.env.MONGO_URL || '',
   minPoolSize: parseInt(process.env.DB_MIN_POOL_SIZE || '5'),
   maxPoolSize: parseInt(process.env.DB_MAX_POOL_SIZE || '10'),
 };
 
-export const corsUrl = process.env.CORS_URL;
+export const corsUrl = process.env.CORS_URL || '*';
 
 export const tokenInfo = {
   accessTokenValidity: parseInt(process.env.ACCESS_TOKEN_VALIDITY_SEC || '0'),
@@ -23,15 +19,3 @@ export const tokenInfo = {
 };
 
 export const logDirectory = process.env.LOG_DIR;
-
-export const redis = {
-  host: process.env.REDIS_HOST || '',
-  port: parseInt(process.env.REDIS_PORT || '0'),
-  password: process.env.REDIS_PASSWORD || '',
-};
-
-export const caching = {
-  contentCacheDuration: parseInt(
-    process.env.CONTENT_CACHE_DURATION_MILLIS || '600000',
-  ),
-};
--- src/database/index.ts
+++ src/database/index.ts
@@ -2,11 +2,7 @@ import mongoose from 'mongoose';
 import Logger from '@/core/Logger';
 import { db } from '@/config';
 
-// Build the connection string
-// const dbURI = `mongodb://${db.user}:${encodeURIComponent(db.password)}@${
-//   db.host
-// }/${db.name}`;
-const dbURI = 'mongodb+srv://hoanghy:01122003@data.nylr2.mongodb.net/etour';
+const dbURI = db.connectionString;
 
 const options = {
   autoIndex: true,
