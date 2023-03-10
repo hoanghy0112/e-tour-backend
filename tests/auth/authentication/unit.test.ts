33ad5ac8157b94541345fc9f1a6e877918e7a5c9
Hoang Hy <hoanghy01122003@gmail.com>
2023-03-10 22:32:23 +0700

    update package.json

--- tests/auth/authentication/unit.test.ts
+++ tests/auth/authentication/unit.test.ts
@@ -2,10 +2,15 @@ import supertest from 'supertest';
 
 import app from '../../../src/app';
 import { addHeaders, getAccessTokenSpy } from './mock';
+import { connection } from '../../../src/database';
 
 describe('user authentication validation', () => {
   const endpoint = '/user/profile';
   const request = supertest(app);
+
+  afterAll(() => {
+    connection.close();
+  });
 
   it('Should response 400 if Authorization header is not passed', async () => {
     const response = await addHeaders(request.get(endpoint));
