9b44a941aff2dd9412188679d4857b7a45c9b40c
Hoang Hy <hoanghy01122003@gmail.com>
2023-03-12 01:33:02 +0700

    debug

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,10 @@ import Logger from './core/Logger';
 import { port } from './config';
 import app from './app';
 
+Logger.info({ env: process.env });
+
 app
   .listen(port, () => {
-    Logger.info({ env: process.env });
     Logger.info(`server running on port : ${port}`);
   })
   .on('error', (e) => Logger.error(e));
