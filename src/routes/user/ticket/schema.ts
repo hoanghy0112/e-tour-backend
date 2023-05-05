5281cd3302df2f5dc6691be75b01b06749694010
Hoang Hy <hoanghy01122003@gmail.com>
2023-05-05 20:50:30 +0700

    fix: change name and age of visitor to optional when book ticket

--- src/routes/user/ticket/schema.ts
+++ src/routes/user/ticket/schema.ts
@@ -6,24 +6,24 @@ export default {
     ticketInfo: Joi.object().keys({
       // userId: Joi.string().required(),
       tourId: Joi.string().required(),
-      fullName: Joi.string().optional().allow("").default(''),
-      email: Joi.string().optional().allow("").default(''),
+      fullName: Joi.string().optional().allow('').default(''),
+      email: Joi.string().optional().allow('').default(''),
       phoneNumber: Joi.string().required(),
       status: Joi.string()
         .valid(...Object.values(PaymentStatus))
         .optional()
         .default(PaymentStatus.CHECKED_OUT),
-      specialRequirement: Joi.string().optional().allow("").default(''),
-      pickupLocation: Joi.string().optional().allow("").default(''),
+      specialRequirement: Joi.string().optional().allow('').default(''),
+      pickupLocation: Joi.string().optional().allow('').default(''),
       visitors: Joi.array()
         .optional()
         .items(
           Joi.object().keys({
-            name: Joi.string().required(),
-            age: Joi.number().required(),
-            address: Joi.string().optional().allow("").default(''),
-            phoneNumber: Joi.string().optional().allow("").default(''),
-            request: Joi.string().optional().allow("").default(''),
+            name: Joi.string().optional().allow('').default(''),
+            age: Joi.number().optional().allow('').default(0),
+            address: Joi.string().optional().allow('').default(''),
+            phoneNumber: Joi.string().optional().allow('').default(''),
+            request: Joi.string().optional().allow('').default(''),
           }),
         ),
     }),
