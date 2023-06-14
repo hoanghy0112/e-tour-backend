9e1391f59ac3c2eb18bb89c95aaa219937de29de
HY <hoanghy01122003@gmail.com>
2023-06-14 09:44:36 +0700

    feat: socket io admin-ui

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import { connectMongo } from './database';
 import { runHttpServer, runSocketServer } from './runServer';
+import os from 'os';
+// import osUtils from 'os-utils'
 
 async function startServer() {
   await connectMongo();
   await runSocketServer();
   await runHttpServer();
+
+  // console.log();
+  console.log(os.totalmem());
+  console.log(os.freemem());
 }
 
 startServer();
--- src/socketServer.ts
+++ src/socketServer.ts
@@ -1,5 +1,6 @@
 import { Server } from 'socket.io';
 import server from './httpServer';
+import { instrument } from '@socket.io/admin-ui';
 
 const io = new Server(server, {
   maxHttpBufferSize: 1e8,
@@ -8,6 +9,11 @@ const io = new Server(server, {
     credentials: true,
     origin: true,
   },
+});
+
+instrument(io, {
+  namespaceName: '/admin',
+  auth: false,
 });
 
 export default io;
