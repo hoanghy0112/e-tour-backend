b80ce96c233408644bc21a15f017718ef42706ca
HY <hoanghy01122003@gmail.com>
2023-06-16 05:31:54 +0700

    feat: upload image by url

--- src/database/s3.ts
+++ src/database/s3.ts
@@ -8,6 +8,7 @@ import {
 import { Bucket, region } from '../config';
 import { v4 as uuidv4 } from 'uuid';
 import { Readable } from 'stream';
+import axios from 'axios';
 
 export const client = new S3Client({
   region: region,
@@ -28,13 +29,44 @@ export async function getS3Image(
   }
 }
 
+export async function uploadImageToS3FromURL(
+  imageURL: string,
+  Key = '',
+): Promise<string | null> {
+  const uid = uuidv4();
+
+  const response = await axios.get(imageURL, { responseType: 'arraybuffer' });
+  const buffer = Buffer.from(response.data, 'utf-8');
+
+  const image = {
+    originalname: imageURL,
+    buffer: buffer,
+  };
+
+  const ext = image.originalname.split('.').at(-1);
+  const imageFileName = `${uid}.${ext}`;
+
+  const command = new PutObjectCommand({
+    Bucket: Bucket,
+    Key: Key || imageFileName,
+    Body: image.buffer,
+  });
+
+  try {
+    await client.send(command);
+    return imageFileName;
+  } catch (error) {
+    return null;
+  }
+}
+
 export async function uploadImageToS3(
   image: { originalname: string; buffer: Buffer },
   Key = '',
 ): Promise<string | null> {
   const uid = uuidv4();
 
-  if ((typeof image) === 'string') return image.toString();
+  if (typeof image === 'string') return image.toString();
 
   const ext = image.originalname.split('.').at(-1);
   const imageFileName = `${uid}.${ext}`;
--- src/routes/imageStorage/image.ts
+++ src/routes/imageStorage/image.ts
@@ -5,7 +5,12 @@ import asyncHandler from '../../helpers/asyncHandler';
 
 import { GetObjectCommand } from '@aws-sdk/client-s3';
 import { Bucket } from '../../config';
-import { client, getS3Image, uploadImageToS3 } from '../../database/s3';
+import {
+  client,
+  getS3Image,
+  uploadImageToS3,
+  uploadImageToS3FromURL,
+} from '../../database/s3';
 import { BadRequestResponse, SuccessResponse } from '../../core/ApiResponse';
 import path from 'path';
 import Logger from '../../core/Logger';
@@ -13,7 +18,7 @@ import { createReadStream, createWriteStream, rmSync, unlinkSync } from 'fs';
 import { Readable } from 'stream';
 import { BadRequestError } from '../../core/ApiError';
 import multer from 'multer';
-import { FileRequest } from '../../types/app-request';
+import { FileRequest, PublicRequest } from '../../types/app-request';
 
 const imageRouter = express.Router();
 
@@ -29,9 +34,6 @@ imageRouter.get(
       const data = await getS3Image(filename);
 
       if (!data) throw new BadRequestError('File not found');
-
-      const filePath = path.join(__dirname, `temp.${filename.split('.')[1]}`);
-      const writable = createWriteStream(filePath);
 
       const body = data.Body as Readable;
 
@@ -49,6 +51,22 @@ imageRouter.post(
     try {
       const image = req.file;
       const imageId = image ? await uploadImageToS3(image) : '';
+      return new SuccessResponse('Upload image successfully', { imageId }).send(
+        res,
+      );
+    } catch (e) {
+      return new BadRequestResponse(JSON.stringify(e)).send(res);
+    }
+  }),
+);
+
+imageRouter.post(
+  '/:imageURL',
+  asyncHandler(async (req: PublicRequest, res) => {
+    try {
+      const { imageURL } = req.params;
+      console.log({ imageURL });
+      const imageId = imageURL ? await uploadImageToS3FromURL(imageURL) : '';
       return new SuccessResponse('Upload image successfully', { imageId }).send(
         res,
       );
