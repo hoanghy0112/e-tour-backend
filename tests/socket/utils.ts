8bb21cac4ec9fbcd5e67a023e6c63dc9dad20038
HY <hoanghy01122003@gmail.com>
2023-06-18 17:36:57 +0700

    fix

--- tests/socket/utils.ts
+++ tests/socket/utils.ts
@@ -9,10 +9,67 @@ import { v4 } from 'uuid';
 import UserModel from '../../src/database/model/User/User';
 import { StaffModel } from '../../src/database/model/Company/Staff';
 
+let clientSocket;
+let clientId;
+
+let staffSocket;
+let staffId;
+
 export async function setupTestedSocketServer() {
   await connectMongo();
   await runSocketServer();
   await runHttpServer();
+
+  const request = supertest(app);
+  await UserModel.deleteMany({});
+  await StaffModel.deleteMany({});
+  const username = 'username';
+  const password = 'password';
+
+  let token = '';
+  let id = '';
+  let response;
+
+  response = await request
+    .post('/user/signup/basic')
+    .field('username', username)
+    .field('password', password)
+    .field('fullName', 'full name')
+    .field('identity', '00000000')
+    .field('isForeigner', false)
+    .field('email', 'email@gmail.com')
+    .field('address', 'Tay Son');
+  const clientToken = response.body.data.tokens.accessToken;
+  clientId = response.body.data.user._id;
+
+  response = await request
+    .post('/company/signup/basic')
+    .field('name', 'congty')
+    .field('email', 'congty@gmail.com')
+    .field('username', username)
+    .field('password', password);
+  staffId = response.body.data.createdAdmin.companyId;
+  const staffToken = response.body.data.tokens.accessToken;
+
+  clientSocket = io(`http://localhost:${process.env.PORT}`, {
+    path: '/socket',
+    query: {
+      type: 'client',
+      token,
+    },
+  });
+
+  staffSocket = io(`http://localhost:${process.env.PORT}`, {
+    path: '/socket',
+    query: {
+      type: 'staff',
+      token,
+    },
+  });
+
+  await new Promise<void>((resolve) => {
+    clientSocket.on('connect', () => resolve());
+  });
 }
 
 export async function cleanUpSocketServer() {
@@ -25,56 +82,14 @@ export async function getSocketInstance(type: 'client' | 'staff'): Promise<{
   socket: Socket;
   id: string;
 }> {
-  const request = supertest(app);
-  await UserModel.deleteMany({});
-  await StaffModel.deleteMany({});
-  const username = 'username';
-  const password = 'password';
-
-  let token = '';
-  let id = '';
-  let response;
-
-  switch (type) {
-    case 'client':
-      response = await request
-        .post('/user/signup/basic')
-        .field('username', username)
-        .field('password', password)
-        .field('fullName', 'full name')
-        .field('identity', '00000000')
-        .field('isForeigner', false)
-        .field('email', 'email@gmail.com')
-        .field('address', 'Tay Son');
-      token = response.body.data.tokens.accessToken;
-      id = response.body.data.user._id;
-      break;
-    case 'staff':
-      response = await request
-        .post('/company/signup/basic')
-        .field('name', 'congty')
-        .field('email', 'congty@gmail.com')
-        .field('username', username)
-        .field('password', password);
-      id = response.body.data.createdAdmin.companyId;
-      token = response.body.data.tokens.accessToken;
-      break;
+  if (type == 'client') {
+    return {
+      socket: clientSocket,
+      id: clientId,
+    };
   }
-
-  const clientSocket = io(`http://localhost:${process.env.PORT}`, {
-    path: '/socket',
-    query: {
-      type,
-      token,
-    },
-  });
-
-  await new Promise<void>((resolve) => {
-    clientSocket.on('connect', () => resolve());
-  });
-
   return {
-    socket: clientSocket,
-    id,
+    socket: staffSocket,
+    id: staffId,
   };
 }
