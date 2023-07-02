ae08942707067ca7820fff35b60d2d0cc1fae701
Tuyet Mai <tranthituyetmai7430@gmail.com>
2023-07-02 21:10:23 +0700

    feat: populate rating to route

--- src/routes/touristRoute/viewTouristRouteByFilter.ts
+++ src/routes/touristRoute/viewTouristRouteByFilter.ts
@@ -1,5 +1,6 @@
 import { SuccessResponse } from '../../core/ApiResponse';
 import TouristsRouteModel from '../../database/model/Company/TouristsRoute';
+import RateRepo from '../../database/repository/User/RateRepo';
 import asyncHandler from '../../helpers/asyncHandler';
 import { PublicRequest } from '../../types/app-request';
 
@@ -8,6 +9,26 @@ export const viewTouristRouteByFilter = asyncHandler(
     const query = req.query;
 
     const data = await TouristsRouteModel.find(query);
+
+    const dataWithRating = await Promise.all(
+      data.map(async (route) => ({
+        ...route.toObject(),
+        ...(await RateRepo.getOverallRatingOfRoute(route._id)),
+      })),
+    );
+
+    return new SuccessResponse('success', dataWithRating).send(res);
+  },
+);
+
+export const viewTouristRouteByDestination = asyncHandler(
+  async (req: PublicRequest, res) => {
+    const destination = req.query?.destination;
+
+    // const data = await TouristsRouteModel.find(query);
+    const data = TouristsRouteModel.find({
+      route: destination,
+    });
 
     return new SuccessResponse('success', data).send(res);
   },
