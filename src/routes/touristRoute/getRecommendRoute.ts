45afb65bef4526fb2560c6a315a116e5511da48b
Tuyt Mai <tranthituyetmai7430@gmail.com>
2023-07-07 23:08:42 +0700

    feat: add recommend route

--- src/routes/touristRoute/getRecommendRoute.ts
+++ src/routes/touristRoute/getRecommendRoute.ts
@@ -1,15 +1,21 @@
+import { Types } from 'mongoose';
 import { SuccessResponse } from '../../core/ApiResponse';
 import TouristsRouteModel from '../../database/model/Company/TouristsRoute';
 import asyncHandler from '../../helpers/asyncHandler';
 import { ProtectedUserRequest } from '../../types/app-request';
 
-export const getRecommendRouteOfCompany = asyncHandler(
+export const getRecommendRouteByFilter = asyncHandler(
   async (req: ProtectedUserRequest, res) => {
-    const query = req.query;
+    const { companyId, size = 5, ...query } = req.query;
 
     const data = await TouristsRouteModel.aggregate([
       {
-        $match: query,
+        $match: {
+          companyId: companyId
+            ? new Types.ObjectId(companyId?.toString())
+            : null,
+          ...query,
+        },
       },
       {
         $lookup: {
@@ -51,6 +57,67 @@ export const getRecommendRouteOfCompany = asyncHandler(
       },
       {
         $unset: ['followers', 'rates', 'company.followers'],
+      },
+      {
+        $sample: { size: parseInt(size?.toString()) },
+      },
+    ]);
+
+    return new SuccessResponse('success', data).send(res);
+  },
+);
+
+export const getRecommendRouteByRouteId = asyncHandler(
+  async (req: ProtectedUserRequest, res) => {
+    const { routeId, size = 5 } = req.query;
+
+    const data = await TouristsRouteModel.aggregate([
+      {
+        $match: {},
+      },
+      {
+        $lookup: {
+          from: 'rates',
+          localField: '_id',
+          foreignField: 'touristsRouteId',
+          as: 'rates',
+        },
+      },
+      {
+        $addFields: {
+          rate: {
+            $ifNull: [
+              {
+                $avg: '$rates.star',
+              },
+              0,
+            ],
+          },
+          num: {
+            $size: '$rates',
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: 'companies',
+          localField: 'companyId',
+          foreignField: '_id',
+          as: 'company',
+        },
+      },
+      {
+        $unwind: {
+          path: '$company',
+          includeArrayIndex: '__companyIndex',
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $unset: ['followers', 'rates', 'company.followers'],
+      },
+      {
+        $sample: { size: parseInt(size?.toString()) },
       },
     ]);
 
--- src/routes/touristRoute/index.ts
+++ src/routes/touristRoute/index.ts
@@ -25,19 +25,22 @@ import { handleViewNotificationOfTour } from '../user/notification/notification'
 import authorization from '../../auth/authorization';
 import { StaffPermission } from '../../database/model/Company/Staff';
 import { getAllRoutes } from './getAllRoutes';
+import {
+  getRecommendRouteByFilter,
+  getRecommendRouteByRouteId,
+} from './getRecommendRoute';
 
 export const touristRouteRouter = express.Router();
+
+touristRouteRouter.get('/find', viewTouristRouteByFilter);
+
+touristRouteRouter.get('/recommend', getRecommendRouteByFilter);
+touristRouteRouter.get('/recommend/similar', getRecommendRouteByRouteId);
 
 touristRouteRouter.get(
   '/',
   authorization([StaffPermission.SUPER_ADMIN]),
   getAllRoutes,
-);
-
-touristRouteRouter.get(
-  '/find',
-  // validator(schema.filter, ValidationSource.QUERY),
-  viewTouristRouteByFilter,
 );
 
 touristRouteRouter.post(
